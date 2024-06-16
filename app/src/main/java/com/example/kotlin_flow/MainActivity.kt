8cb592808734c34ea0fde4ae80cd94354fe2548c
Soumen Pal <soumenpal3241@gmail.com>
2024-06-16 16:42:17 +0530

    Channel in kotlin

---
+++ app/src/main/java/com/example/kotlin_flow/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.kotlin_flow
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlin_flow.ui.theme.Kotlin_flowTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    val channel = Channel<Int>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Kotlin_flowTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    flow(modifier = Modifier.padding(innerPadding) , channel)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun flow(modifier: Modifier , channel: Channel<Int>){
+
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+
+            producer(channel)
+            consumer(channel)
+
+
+        }) {
+            Text(text = "Click")
+        }
+    }
+
+}
+
+fun producer(channel: Channel<Int>){
+    CoroutineScope(Dispatchers.Main).launch {
+        for(i in 1..5){
+            channel.send(i)
+        }
+    }
+}
+
+fun consumer(channel: Channel<Int>){
+    CoroutineScope(Dispatchers.Main).launch {
+        for(i in 1..5){
+            delay(1000)
+            val d = channel.receive()
+            Log.d("data" , "Data:$d")
+        }
+    }
+}
+
+
